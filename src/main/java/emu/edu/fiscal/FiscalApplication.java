53464f508b1234bb7e6d054e59261a85e58758ef
Hoa Ho <hoa.hoxuan900@gmail.com>
2023-01-26 09:51:22 -0500

    Login Scene UI
    Login scene UI

--- src/main/java/emu/edu/fiscal/FiscalApplication.java
+++ src/main/java/emu/edu/fiscal/FiscalApplication.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class FiscalApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FiscalApplication.class.getResource("/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(FiscalApplication.class.getResource("/LoginHH.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add("LoginHH.css");
+
+        stage.setTitle("Fiscal Management System");
+        stage.setResizable(false);
+        //stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/emu/edu/fiscal/controller/LoginController.java
@@ -0,0 +1,7 @@
+package emu.edu.fiscal.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+}
